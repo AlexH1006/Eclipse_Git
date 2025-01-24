86d92dcb7245eb283f5985c519b94e2e48377c0f
AlexH1006 <alexhermay06@gmail.com>
2025-01-24 16:38:22 +0100

    Una multiplicacion de una variable

--- src/HolaMundo/HolaMundo.java
+++ src/HolaMundo/HolaMundo.java
@@ -5,9 +5,12 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int numero = 1;
 		System.out.println("Hola Mundo");
 		System.out.println(2+1);
 		System.out.println("Alejandro Hernanz Mayayo");
+		System.out.println(numero * 10);
+		
 	}
 
 }
